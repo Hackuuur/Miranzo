0303944d380077a4db589bee64ab25e5159b3443
Urmin <87514909+Urminhirpara@users.noreply.github.com>
2023-10-01 16:06:18 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,48 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import {
+  ThirdwebProvider,
+  ConnectWallet,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  localWallet,
+  embeddedWallet,
+  trustWallet,
+  zerionWallet,
+  rainbowWallet,
+  phantomWallet,
+} from "@thirdweb-dev/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import "../styles/globals.css";
-
+import { Navbar } from "../components/Navbar";
+import theme from "./theme"
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
-      clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
-    >
+      activeChain="mumbai"
+      clientId="1e83bfebeb5ca87de89692857d336e9a"
+      supportedWallets={[
+        metamaskWallet({ recommended: true }),
+        coinbaseWallet(),
+        walletConnect(),
+        localWallet(),
+        embeddedWallet({ recommended: true }),
+        trustWallet(),
+        zerionWallet(),
+        rainbowWallet(),
+        phantomWallet(),
+      ]}
+    >  <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Navbar />
       <Component {...pageProps} />
-    </ThirdwebProvider>
+    </ChakraProvider>
+  </ThirdwebProvider>
   );
 }
 
